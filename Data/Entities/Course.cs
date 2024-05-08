8a3d8785e547ca16ed19522a6b95dd823b0752e3
Fadi-Alyonan <fadialyonan@gmail.com>
2024-05-09 01:45:08 +0200

    Add project files.

---
+++ Data/Entities/Course.cs
@@ -0,0 +1,16 @@
+﻿namespace Data.Entities;
+
+public class Course
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string Image { get; set; } = null!;
+    public string Title { get; set; } = null!;
+    public string Author { get; set; } = null!;
+    public string OriginalPrice { get; set; } = null!;
+    public string? DiscountPrice { get; set; }
+    public int Hours { get; set; }
+    public string? LikesInProcent { get; set; }
+    public string? NumberOfLikes { get; set; }
+    public bool IsDigital { get; set; } = false;
+    public bool IsBestseller { get; set; } = false;
+}
